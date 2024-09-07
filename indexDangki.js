e23da6399b36c5f66c5698235f24b60caf1581e5
AnhTrung1997 <trananhtrungtm@gmail.com>
2024-09-07 23:36:23 +0700

    s

--- indexDangki.js
+++ indexDangki.js
@@ -4,7 +4,7 @@ nutdangki.onclick = function () {
     let taikhoan = document.getElementById("tkdangki").value
     let pass = document.getElementById("passdangki").value
     if (taikhoan.length < 6 || pass.length < 6) {
-        alert("Vui lòng nhập tên tài khoản & Mật khẩu tối tiểu 6 ký tự")
+        alert("Vui lòng nhập tên Tài khoản & Mật khẩu tối thiểu 6 ký tự")
     }
     else {
         nutdangki.setAttribute("href", "./indexlogin.html")
